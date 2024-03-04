4a2e7996154ca7fce939d953531c56726c644fdd
Christopher Cortes <christopherctes@hotmail.com>
2024-03-04 19:04:02 -0300

    week 1 assignment

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port, () =>
-      console.log(`Database is listening and node running on port ${port}`));
+    app.listen(port, () => {
+      console.log(`Database is listening and node running on port ${port}`);
+    });
   }
 });
